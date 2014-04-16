987c2aa0624cedbfe60710ca872eaaa2eec60151
Seemanta Dutta <seemanta@codeaurora.org>
2014-04-15 17:46:21 -0700

    msm: camera: cpp: Fix ioctl handler to return correct status
    Fix the msm_cpp_subdev_fops_compat_ioctl() function so that it
    returns the proper status.
    
    Change-Id: I95f12b9f6b18a406b79e1dec79422f3c7ed834dd
    Signed-off-by: Seemanta Dutta <seemanta@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/pproc/cpp/msm_cpp.c
+++ drivers/media/platform/msm/camera_v2/pproc/cpp/msm_cpp.c
@@ -2322,7 +2322,7 @@ static long msm_cpp_subdev_fops_compat_ioctl(struct file *file,
 	if (copy_to_user((void __user *)up, &up32_ioctl, sizeof(up32_ioctl)))
 		return -EFAULT;
 
-	return 0;
+	return rc;
 }
 #endif
 
