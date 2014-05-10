73fdc4cedc64b98a764405e59f347f41b180891b
Huaibin Yang <huaibiny@codeaurora.org>
2014-05-09 19:58:25 -0700

    msm: mdss: debugfs: xlog: move xlog related variables to local
    debug_log is an unnecessary global structure that is actually only
    related to xlog debugfs nodes. Those variables that control xlog
    behaviors should be in the local xlog file.
    
    Change-Id: I8e2a37e72d849b09e5c8afb8b2170adf9516e215
    Signed-off-by: Huaibin Yang <huaibiny@codeaurora.org>
    [prototype74: backported to msm8916; picked from LA.BR.1.3.7.c26]

--- drivers/video/msm/mdss/mdss_debug_xlog.c
+++ drivers/video/msm/mdss/mdss_debug_xlog.c
@@ -42,6 +42,10 @@ struct mdss_dbg_xlog {
 	int first;
 	int last;
 	spinlock_t xlock;
+	struct dentry *xlog;
+	u32 xlog_enable;
+	u32 panic_on_err;
+	u32 enable_reg_dump;
 } mdss_dbg_xlog;
 
 static int mdss_xlog_dump_open(struct inode *inode, struct file *file)
@@ -68,38 +72,33 @@ int mdss_create_xlog_debug(struct mdss_debug_data *mdd)
 {
 	spin_lock_init(&mdss_dbg_xlog.xlock);
 
-	mdd->logd.xlog = debugfs_create_dir("xlog", mdd->root);
-	if (IS_ERR_OR_NULL(mdd->logd.xlog)) {
+	mdss_dbg_xlog.xlog = debugfs_create_dir("xlog", mdd->root);
+	if (IS_ERR_OR_NULL(mdss_dbg_xlog.xlog)) {
 		pr_err("debugfs_create_dir fail, error %ld\n",
-		       PTR_ERR(mdd->logd.xlog));
-		mdd->logd.xlog = NULL;
+		       PTR_ERR(mdss_dbg_xlog.xlog));
+		mdss_dbg_xlog.xlog = NULL;
 		return -ENODEV;
 	}
-
-	mdd->logd.xlog_enable = true;
-
-	debugfs_create_file("dump", 0644, mdd->logd.xlog, NULL,
+	debugfs_create_file("dump", 0644, mdss_dbg_xlog.xlog, NULL,
 						&mdss_xlog_fops);
-	debugfs_create_bool("enable", 0644, mdd->logd.xlog,
-			    &mdd->logd.xlog_enable);
-	debugfs_create_bool("panic", 0644, mdd->logd.xlog,
-			    &mdd->logd.panic_on_err);
-	debugfs_create_bool("reg_dump", 0644, mdd->logd.xlog,
-			    &mdd->logd.enable_reg_dump);
+	debugfs_create_bool("enable", 0644, mdss_dbg_xlog.xlog,
+			    &mdss_dbg_xlog.xlog_enable);
+	debugfs_create_bool("panic", 0644, mdss_dbg_xlog.xlog,
+			    &mdss_dbg_xlog.panic_on_err);
+	debugfs_create_bool("reg_dump_in_log", 0644, mdss_dbg_xlog.xlog,
+			    &mdss_dbg_xlog.enable_reg_dump);
 	return 0;
 }
 
 void mdss_xlog(const char *name, ...)
 {
-	struct mdss_data_type *mdata = mdss_mdp_get_mdata();
-	struct mdss_debug_data *mdd = mdata->debug_inf.debug_data;
 	unsigned long flags;
 	int i, val = 0;
 	va_list args;
 	struct tlog *log;
 	ktime_t time;
 
-	if (!mdd->logd.xlog_enable)
+	if (!mdss_dbg_xlog.xlog_enable)
 		return;
 
 	spin_lock_irqsave(&mdss_dbg_xlog.xlock, flags);
@@ -133,15 +132,13 @@ void mdss_xlog(const char *name, ...)
 
 void mdss_xlog_dump(void)
 {
-	struct mdss_data_type *mdata = mdss_mdp_get_mdata();
-	struct mdss_debug_data *mdd = mdata->debug_inf.debug_data;
 	int i, n, d_cnt, off;
 	unsigned long flags;
 	unsigned long rem_nsec;
 	struct tlog *log;
 	char xlog_buf[MDSS_XLOG_BUF_MAX];
 
-	if (!mdd->logd.xlog_enable)
+	if (!mdss_dbg_xlog.xlog_enable)
 		return;
 
 	spin_lock_irqsave(&mdss_dbg_xlog.xlock, flags);
@@ -178,7 +175,7 @@ void mdss_xlog_tout_handler(const char *name, ...)
 	char *dsi1_addr = NULL;
 #endif
 
-	if (!mdd->logd.xlog_enable)
+	if (!mdss_dbg_xlog.xlog_enable)
 		return;
 
 #if defined(CONFIG_FB_MSM_MDSS_SAMSUNG)
@@ -198,7 +195,7 @@ void mdss_xlog_tout_handler(const char *name, ...)
 
 			if (blk_base->name &&
 				!strcmp(blk_base->name, blk_name) &&
-				mdd->logd.enable_reg_dump) {
+				mdss_dbg_xlog.enable_reg_dump) {
 				pr_info("\n%s  :   =========%s DUMP=========\n",
 						__func__, blk_base->name);
 				mdss_dump_reg(blk_base->base,
@@ -236,6 +233,6 @@ void mdss_xlog_tout_handler(const char *name, ...)
 		panic(name);
 #endif
 
-	if (dead && mdd->logd.panic_on_err)
+	if (dead && mdss_dbg_xlog.panic_on_err)
 		panic(name);
 }
