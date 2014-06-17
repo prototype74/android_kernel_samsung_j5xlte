d63f813bc8dba0c2d5bfd24cd0e6cfc736c85606
Petar Sivenov <psiven@codeaurora.org>
2014-06-17 16:42:48 -0700

    msm: camera: isp: fix isp bus clock calculation
    ISP send too big numbers for bus update, which were discarded and
    clock was not updated. This fix corrects ISP bus clock result, using
    proper 64bit operations.
    
    Change-Id: I763d8a508283f74fc0a8538e23a8f81facbc6d3a
    Signed-off-by: Petar Sivenov <psiven@codeaurora.org>
    [prototype74: removed min_ib set by Samsung]

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -26,10 +26,6 @@
 
 #define HANDLE_TO_IDX(handle) (handle & 0xFF)
 
-#define MSM_ISP_MIN_AB 450000000
-#define MSM_ISP_MIN_IB 900000000
-#define MIN_IB         1700000000
-
 int msm_isp_axi_create_stream(
 	struct msm_vfe_axi_shared_data *axi_data,
 	struct msm_vfe_axi_stream_request_cmd *stream_cfg_cmd)
@@ -1265,12 +1261,9 @@ static int msm_isp_update_stream_bandwidth(struct vfe_device *vfe_dev)
 	int i, rc = 0;
 	struct msm_vfe_axi_stream *stream_info;
 	struct msm_vfe_axi_shared_data *axi_data = &vfe_dev->axi_data;
-	uint32_t total_pix_bandwidth = 0, total_rdi_bandwidth = 0;
+	uint64_t total_pix_bandwidth = 0, total_rdi_bandwidth = 0;
 	uint32_t num_pix_streams = 0;
-	uint32_t num_rdi_streams = 0;
-	uint32_t total_streams   = 0;
 	uint64_t total_bandwidth = 0;
-	uint64_t ib_total_bandwidth = 0;
 
 	for (i = 0; i < MAX_NUM_STREAM; i++) {
 		stream_info = &axi_data->stream_info[i];
@@ -1281,33 +1274,22 @@ static int msm_isp_update_stream_bandwidth(struct vfe_device *vfe_dev)
 				num_pix_streams++;
 			} else {
 				total_rdi_bandwidth += stream_info->bandwidth;
-				num_rdi_streams++;
 			}
 		}
 	}
 	if (num_pix_streams > 0)
-		total_pix_bandwidth = total_pix_bandwidth /
-			num_pix_streams * (num_pix_streams - 1) +
-			((unsigned long)axi_data->src_info[VFE_PIX_0].
-			pixel_clock) * ISP_DEFAULT_FORMAT_FACTOR / ISP_Q2;
+		do_div(total_pix_bandwidth, num_pix_streams);
+
+	total_pix_bandwidth = total_pix_bandwidth *
+		(uint64_t)(num_pix_streams - 1);
+	total_pix_bandwidth +=
+		((uint64_t)axi_data->src_info[VFE_PIX_0].pixel_clock *
+		(uint64_t)(ISP_DEFAULT_FORMAT_FACTOR)) / (uint64_t)ISP_Q2;
+
 	total_bandwidth = total_pix_bandwidth + total_rdi_bandwidth;
-	total_streams = num_pix_streams + num_rdi_streams;
-	if (total_streams == 1) {
-		ib_total_bandwidth = total_bandwidth *
-			ISP_BUS_UTILIZATION_FACTOR / ISP_Q2 - MSM_ISP_MIN_IB;
-		if(ib_total_bandwidth < MIN_IB)
-			ib_total_bandwidth = MIN_IB;
-		rc = msm_isp_update_bandwidth(ISP_VFE0 + vfe_dev->pdev->id,
-			(total_bandwidth - MSM_ISP_MIN_AB) , ib_total_bandwidth);
-	}
-	else {
-		ib_total_bandwidth = total_bandwidth *
-			ISP_BUS_UTILIZATION_FACTOR / ISP_Q2;
-		if(ib_total_bandwidth < MIN_IB)
-			ib_total_bandwidth = MIN_IB;
-		rc = msm_isp_update_bandwidth(ISP_VFE0 + vfe_dev->pdev->id,
-			total_bandwidth, ib_total_bandwidth);
-	}
+	rc = msm_isp_update_bandwidth(ISP_VFE0 + vfe_dev->pdev->id,
+		total_bandwidth,  total_bandwidth *
+		ISP_BUS_UTILIZATION_FACTOR / ISP_Q2);
 	if (rc < 0)
 		pr_err("%s: update failed\n", __func__);
 
