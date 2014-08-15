4ff516efd2f74cf45251fd7fc98bafd8ac2e92e6
Peter Liu <pingchie@codeaurora.org>
2014-08-14 22:09:18 -0700

    msm: camera: fix AXI RESET cmd field to be 32 byte
    In 32 bit userspace and 64 bit kernel,
    using long could cause the bit lenght difference in kernel and userspace.
    change to use uint32_t instead of long.
    
    Change-Id: I2f3d8f30074b444e2919b53ddb0247da5f97baa9
    Signed-off-by: Peter Liu <pingchie@codeaurora.org>

--- include/media/msmb_isp.h
+++ include/media/msmb_isp.h
@@ -219,7 +219,7 @@ struct msm_vfe_axi_halt_cmd {
 
 struct msm_vfe_axi_reset_cmd {
 	uint32_t blocking; //Boolean whether stop camif is to be done
-	unsigned long frame_id;
+	uint32_t frame_id;
 };
 
 struct msm_vfe_axi_restart_cmd {
