f143ceb1bcfa3e435ef57d3e4190ae2cf502bcb5
c_psiven <psiven@codeaurora.org>
2014-09-05 15:19:26 +0300

    msm: camera: isp: fix for burst frame drop
    This change fixes issue in which ISP driver fails to send all
    requested frames in burst mode when frame skip is updated runtime. The
    root cause is that only frame skip value was modified, but not the
    total number of frames driver is waiting for. Now when frame skip
    request is received, ISP driver recalculates the total number of
    frames to wait and updates frame skip on next reg update.
    
    Change-Id: I7722f5d828e20fcccb0b3b2b8db0276fd28a5bc8
    Signed-off-by: c_psiven <psiven@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp.h
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp.h
@@ -342,7 +342,8 @@ struct msm_vfe_axi_stream {
 	uint32_t runtime_init_frame_drop;
 	uint32_t runtime_burst_frame_count;/*number of sof before burst stop*/
 	uint32_t runtime_num_burst_capture;
-	uint8_t runtime_framedrop_update;
+	uint8_t  runtime_framedrop_update;
+	uint8_t  runtime_framedrop_update_burst;
 	uint32_t runtime_output_format;
 	enum msm_vfe_frame_skip_pattern frame_skip_pattern;
 
