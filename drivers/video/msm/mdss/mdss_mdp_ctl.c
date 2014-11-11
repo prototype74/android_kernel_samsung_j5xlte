a79e40f341ddfa49caa1862fa9dface58d2b28b1
Ujwal Patel <ujwalp@codeaurora.org>
2014-11-10 18:11:51 -0800

    msm: mdss: workaround for bwc and panic
    Due to HW problem, when BWC is enabled, panic is asserted continuously
    as long as pipe with BWC decode is fetching from the memory. This
    continuous panic assertion causes unnecessary CPU throttling and causes
    lot of side effects. To prevent the issues related to HW problem, disable
    BWC on video mode panels on msm8994. On CMD mode panels, use BWC but
    disable panic signal as long as there is no video mode secondary display.
    It is user-space's responsibility to not use BWC when primary display is
    CMD mode but any kind of secondary video mode display is active.
    
    Change-Id: Idb219e61599d2e2564bd4076fcfc787cd99c8d4d
    Signed-off-by: Ujwal Patel <ujwalp@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_ctl.c
+++ drivers/video/msm/mdss/mdss_mdp_ctl.c
@@ -3432,6 +3432,7 @@ int mdss_mdp_display_commit(struct mdss_mdp_ctl *ctl, void *arg,
 	int ret = 0;
 	bool is_bw_released;
 	int split_enable;
+	struct mdss_data_type *mdata = mdss_mdp_get_mdata();
 
 #if defined(CONFIG_FB_MSM_MDSS_SAMSUNG)
 	struct mdss_overlay_private *mdp5_data = NULL;
@@ -3568,6 +3569,10 @@ int mdss_mdp_display_commit(struct mdss_mdp_ctl *ctl, void *arg,
 	if (commit_cb)
 		commit_cb->commit_cb_fnc(MDP_COMMIT_STAGE_READY_FOR_KICKOFF,
 			commit_cb->data);
+
+	if (mdss_has_quirk(mdata, MDSS_QUIRK_BWCPANIC) &&
+	    !bitmap_empty(mdata->bwc_enable_map, MAX_DRV_SUP_PIPES))
+		mdss_mdp_bwcpanic_ctrl(mdata, true);
 
 	ATRACE_BEGIN("flush_kickoff");
 	mdss_mdp_ctl_write(ctl, MDSS_MDP_REG_CTL_FLUSH, ctl->flush_bits);
