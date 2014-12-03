6cdba3767329aebc8b6af646cf4cdb79597db091
Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
2014-12-03 10:18:12 -0800

    sched: Make RT tasks eligible for boost
    During sched boost RT tasks currently end up going to the lowest
    power cluster. This can be a performance bottleneck especially if
    the frequency and IPC differences between clusters are high.
    Furthermore, when RT tasks go over to the little cluster during
    boost, the load balancer keeps attempting to pull work over to the
    big cluster. This results in pre-emption of the executing RT task
    causing more delays. Finally, containing more work on a single
    cluster during boost might help save some power if the little
    cluster can then enter deeper low power modes.
    
    Change-Id: I177b2e81be5657c23e7ac43889472561ce9993a9
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -749,6 +749,11 @@ static inline u64 scale_load_to_cpu(u64 task_load, int cpu)
 	return task_load;
 }
 
+static inline int capacity(struct rq *rq)
+{
+	return rq->capacity;
+}
+
 static inline void
 inc_cumulative_runnable_avg(struct rq *rq, struct task_struct *p)
 {
@@ -778,6 +783,11 @@ static inline unsigned int nr_eligible_big_tasks(int cpu)
 }
 
 static inline int pct_task_load(struct task_struct *p) { return 0; }
+
+static inline int capacity(struct rq *rq)
+{
+	return SCHED_LOAD_SCALE;
+}
 
 static inline void
 inc_cumulative_runnable_avg(struct rq *rq, struct task_struct *p)
@@ -873,6 +883,7 @@ extern void set_hmp_defaults(void);
 extern unsigned int power_cost_at_freq(int cpu, unsigned int freq);
 extern void reset_all_window_stats(u64 window_start, unsigned int window_size);
 extern void boost_kick(int cpu);
+extern int sched_boost(void);
 
 #else /* CONFIG_SCHED_HMP */
 
