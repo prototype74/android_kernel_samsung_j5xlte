1bb7c8f28e8ea70ba34691d05089f7e374b3e3ad
Ujwal Patel <ujwalp@codeaurora.org>
2014-12-05 18:04:29 -0800

    msm: mdss: xlog: add new xlog messages and remove unnecessary ones
    Add new xlog messages to aid in debugging of MDSS issues. Some of the new
    messages include information regarding mixer configuration, write-back
    configuration at start and finish of the operation, pipe configuration
    and pixel extensions etc. Also remove old mixer reg xlog and report xlog
    from mdp_clk_ctrl only once to prevent flooding.
    
    Change-Id: I11fd63a220b1ccd102dcd48d3878cf96fc91bc5f
    Signed-off-by: Ujwal Patel <ujwalp@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_ctl.c
+++ drivers/video/msm/mdss/mdss_mdp_ctl.c
@@ -24,11 +24,9 @@
 
 #include "mdss_fb.h"
 #include "mdss_mdp.h"
-#include "mdss_debug.h"
 #include "mdss_mdp_trace.h"
 #include "mdss_debug.h"
 
-static void mdss_mdp_xlog_mixer_reg(struct mdss_mdp_ctl *ctl);
 static inline u64 fudge_factor(u64 val, u32 numer, u32 denom)
 {
 	u64 result = (val * (u64)numer);
@@ -3148,6 +3146,7 @@ update_mixer:
 
 	pr_debug("mixer=%d mixer_cfg=0%x mixercfg_extn=0x%x\n",
 		mixer->num, mixercfg, mixercfg_extn);
+	MDSS_XLOG(mixer->num, mixercfg, mixercfg_extn);
 }
 
 int mdss_mdp_mixer_addr_setup(struct mdss_data_type *mdata,
@@ -3732,8 +3731,6 @@ int mdss_mdp_display_commit(struct mdss_mdp_ctl *ctl, void *arg,
 	}
 	ATRACE_END("postproc_programming");
 
-	mdss_mdp_xlog_mixer_reg(ctl);
-
 	ATRACE_BEGIN("frame_ready");
 	mdss_mdp_ctl_notify(ctl, MDP_NOTIFY_FRAME_CFG_DONE);
 	if (commit_cb)
@@ -4090,19 +4087,4 @@ int mdss_mdp_mixer_handoff(struct mdss_mdp_ctl *ctl, u32 num,
 	}
 
 	return rc;
-}
-
-static void mdss_mdp_xlog_mixer_reg(struct mdss_mdp_ctl *ctl)
-{
-	int i, off;
-	u32 data[MDSS_MDP_INTF_MAX_LAYERMIXER];
-
-	for (i = 0; i < MDSS_MDP_INTF_MAX_LAYERMIXER; i++) {
-		off =  MDSS_MDP_REG_CTL_LAYER(i);
-		data[i] = mdss_mdp_ctl_read(ctl, off);
-	}
-	MDSS_XLOG(data[MDSS_MDP_INTF_LAYERMIXER0],
-		data[MDSS_MDP_INTF_LAYERMIXER1],
-		data[MDSS_MDP_INTF_LAYERMIXER2],
-		data[MDSS_MDP_INTF_LAYERMIXER3], off);
 }
