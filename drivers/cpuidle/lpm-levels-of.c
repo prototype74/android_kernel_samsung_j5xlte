01e83e4c293b119b2351abc5e97cd8b48d2ed245
Murali Nalajala <mnalajal@codeaurora.org>
2015-01-07 19:36:57 -0800

    cpu_pm: Add level to the cluster pm notification
    Cluster pm notifications without level information increases difficulty
    and complexity for the registered drivers to figure out when the last
    coherency level is going into power collapse.
    
    Send notifications with level information that allows the registered
    drivers to easily determine the cluster level that is going in/out of
    power collapse.
    
    There is an issue with this implementation. GIC driver saves and
    restores the distributed registers as part of cluster notifications. On
    newer platforms there are multiple cluster levels are defined (e.g l2,
    cci etc). These cluster level notofications can happen independently.
    On MSM platforms GIC is still active while the cluster sleeps in idle,
    causing the GIC state to be overwritten with an incorrect previous state
    of the interrupts. This leads to a system hang. Do not save and restore
    on any L2 and higher cache coherency level sleep entry and exit.
    
    Change-Id: I31918d6383f19e80fe3b064cfaf0b55e16b97eb6
    Signed-off-by: Archana Sathyakumar <asathyak@codeaurora.org>
    Signed-off-by: Murali Nalajala <mnalajal@codeaurora.org>

--- drivers/cpuidle/lpm-levels-of.c
+++ drivers/cpuidle/lpm-levels-of.c
@@ -805,6 +805,7 @@ struct lpm_cluster *parse_cluster(struct device_node *node,
 			list_add(&child->list, &c->child);
 			cpumask_or(&c->child_cpus, &c->child_cpus,
 					&child->child_cpus);
+			c->aff_level = child->aff_level + 1;
 			continue;
 		}
 
@@ -820,6 +821,8 @@ struct lpm_cluster *parse_cluster(struct device_node *node,
 
 			if (parse_cpu_levels(n, c))
 				goto failed_parse_cluster;
+
+			c->aff_level = 1;
 		}
 	}
 
