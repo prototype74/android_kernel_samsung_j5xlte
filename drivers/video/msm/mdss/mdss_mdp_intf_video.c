e08bef10074b3ba8e8353c60f1a40f4c1919ca5c
Ingrid Gallardo <ingridg@codeaurora.org>
2015-01-08 18:16:23 -0800

    msm: mdss: debugfs: xlog: display kernel panic cleanup
    Cleanup the kernel panic within the display driver to
    dump mdp registers before driver panics and remove
    unnecessary panic during underrun interrupt.
    
    Change-Id: Ia8cb125b2836096b4179463cb4d4e6d1bd6cd804
    Signed-off-by: Ingrid Gallardo <ingridg@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_intf_video.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_video.c
@@ -676,7 +676,6 @@ static void mdss_mdp_video_underrun_intr_done(void *arg)
 
 	ctl->underrun_cnt++;
 	MDSS_XLOG(ctl->num, ctl->underrun_cnt);
-	MDSS_XLOG_TOUT_HANDLER_WQ("mdp", "vbif", "panic");
 	trace_mdp_video_underrun_done(ctl->num, ctl->underrun_cnt);
 	pr_info("display underrun detected for ctl=%d count=%d\n", ctl->num,
 			ctl->underrun_cnt);
