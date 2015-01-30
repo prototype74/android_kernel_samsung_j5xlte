8c379bcd6149b25533e2bf133d1796029cbf457f
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2015-01-30 11:52:37 +0530

    sched: Keep track of average nr_big_tasks
    Extend sched_get_nr_running_avg() API to return average nr_big_tasks,
    in addition to average nr_running and average nr_io_wait tasks. Also
    add a new trace point to record values returned by
    sched_get_nr_running_avg() API.
    
    Change-Id: Id3591e6d04da8db484b4d1cb9d95dba075f5ab9a
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2108,6 +2108,30 @@ void dec_nr_big_small_task(struct rq *rq, struct task_struct *p)
 }
 
 /*
+ * Return total number of tasks "eligible" to run on highest capacity cpu
+ *
+ * This is simply nr_big_tasks for cpus which are not of max_capacity and
+ * (nr_running - nr_small_tasks) for cpus of max_capacity
+ */
+unsigned int nr_eligible_big_tasks(int cpu)
+{
+	struct rq *rq = cpu_rq(cpu);
+	int nr_big = rq->nr_big_tasks;
+	int nr = rq->nr_running;
+	int nr_small = rq->nr_small_tasks;
+
+	if (rq->capacity != max_capacity)
+		return nr_big;
+
+	/* Consider all (except small) tasks on max_capacity cpu as big tasks */
+	nr_big = nr - nr_small;
+	if (nr_big < 0)
+		nr_big = 0;
+
+	return nr_big;
+}
+
+/*
  * Walk runqueue of cpu and re-initialize 'nr_big_tasks' and 'nr_small_tasks'
  * counters.
  */
