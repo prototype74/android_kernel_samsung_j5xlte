d0f57800c4d3198d68a26568d451dd0e5f189e6f
Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
2015-02-20 17:09:41 -0800

    sched: Update max_capacity when an entire cluster is hotplugged
    When an entire cluster is hotplugged, the scheduler's notion of
    max_capacity can get outdated. This introduces the following
    inefficiencies in behavior:
    
    * task_will_fit() does not return true on all tasks. Consequently
      all big tasks go through fallback CPU selection logic skipping
      C-state and power checks in select_best_cpu().
    
    * During boost, migration_needed() return true unnecessarily
      causing an avoidable rerun of select_best_cpu().
    
    * An unnecessary kick is sent to all little CPUs when boost is set.
    
    * An opportunity for early bailout from nohz_kick_needed() is lost.
    
    Start handling CPUFREQ_REMOVE_POLICY in the policy notifier callback
    which indicates the last CPU in a cluster being hotplugged out. Also
    modify update_min_max_capacity() to only iterate through online CPUs
    instead of possible CPUs. While we can't guarantee the integrity of
    the cpu_online_mask in the notifier callback, the scheduler will fix
    up all state soon after any changes to the online mask.
    
    The change does have one side effect; early termination from the
    notifier callback when min_max_freq or max_possible_freq remain
    unchanged is no longer possible. This is because when the last CPU
    in a cluster is hot removed, only max_capacity is updated without
    affecting min_max_freq or max_possible_freq. Therefore, when the
    first CPU in the same cluster gets hot added at a later point
    max_capacity must once again be recomputed despite there being no
    change in min_max_freq or max_possible_freq.
    
    Change-Id: I9a1256b5c2cd6fcddd85b069faf5e2ace177e122
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
    [prototype74:
       - backported to msm8916; picked from LA.BR.1.3.7.c26
       - move nr_eligible_big_tasks position
    ]

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1836,7 +1836,7 @@ static int eligible_cpu(struct task_struct *p, int cpu, int sync)
 	if (mostly_idle_cpu_sync(cpu, sync))
 		return 1;
 
-	if (rq->capacity != max_capacity)
+	if (rq->max_possible_capacity != max_possible_capacity)
 		return !spill_threshold_crossed(p, rq, cpu, sync);
 
 	return 0;
@@ -2376,6 +2376,30 @@ static inline void reset_cfs_rq_hmp_stats(int cpu, int reset_cra) { }
 #endif	/* CONFIG_CFS_BANDWIDTH */
 
 /*
+ * Return total number of tasks "eligible" to run on highest capacity cpu
+ *
+ * This is simply nr_big_tasks for cpus which are not of max_capacity and
+ * (nr_running - nr_small_tasks) for cpus of max_capacity
+ */
+unsigned int nr_eligible_big_tasks(int cpu)
+{
+	struct rq *rq = cpu_rq(cpu);
+	int nr_big = rq->hmp_stats.nr_big_tasks;
+	int nr = rq->nr_running;
+	int nr_small = rq->hmp_stats.nr_small_tasks;
+
+	if (rq->max_possible_capacity != max_possible_capacity)
+		return nr_big;
+
+	/* Consider all (except small) tasks on max_capacity cpu as big tasks */
+	nr_big = nr - nr_small;
+	if (nr_big < 0)
+		nr_big = 0;
+
+	return nr_big;
+}
+
+/*
  * reset_cpu_hmp_stats - reset HMP stats for a cpu
  *	nr_big_tasks, nr_small_tasks
  *	cumulative_runnable_avg (iff reset_cra is true)
@@ -2487,30 +2511,6 @@ _inc_hmp_sched_stats_fair(struct rq *rq, struct task_struct *p, int change_cra)
 }
 
 #endif	/* CONFIG_CFS_BANDWIDTH */
-
-/*
- * Return total number of tasks "eligible" to run on highest capacity cpu
- *
- * This is simply nr_big_tasks for cpus which are not of max_capacity and
- * (nr_running - nr_small_tasks) for cpus of max_capacity
- */
-unsigned int nr_eligible_big_tasks(int cpu)
-{
-	struct rq *rq = cpu_rq(cpu);
-	int nr_big = rq->hmp_stats.nr_big_tasks;
-	int nr = rq->nr_running;
-	int nr_small = rq->hmp_stats.nr_small_tasks;
-
-	if (rq->capacity != max_capacity)
-		return nr_big;
-
-	/* Consider all (except small) tasks on max_capacity cpu as big tasks */
-	nr_big = nr - nr_small;
-	if (nr_big < 0)
-		nr_big = 0;
-
-	return nr_big;
-}
 
 /*
  * Walk runqueue of cpu and re-initialize 'nr_big_tasks' and 'nr_small_tasks'
