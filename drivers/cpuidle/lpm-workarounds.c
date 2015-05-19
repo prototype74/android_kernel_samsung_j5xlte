aaead2b645d8fad4fddf172a3a0f7913cd99f62c
Xiaocheng Li <lix@codeaurora.org>
2015-05-19 20:19:16 +0800

    drivers: lpm-workarounds: disable preempt during getting current cpu
    smp_processor_id() should be called in non-preemptible context, so
    use get_cpu() instead to ensure preempt is disabled during getting
    current cpu and setting L1_L2_GCC register.
    
    Change-Id: I749d42e2b77b2c0fe3188b2f468ed8d29909ee23
    Signed-off-by: Xiaocheng Li <lix@codeaurora.org>

--- drivers/cpuidle/lpm-workarounds.c
+++ drivers/cpuidle/lpm-workarounds.c
@@ -80,19 +80,18 @@ static struct notifier_block __refdata lpm_wa_nblk = {
 
 static void process_lpm_workarounds(struct work_struct *w)
 {
-	int ret = 0, status = 0;
-
+	int ret = 0, status = 0, cpu = 0;
 
 	/* MSM8952 have L1/L2 dynamic clock gating disabled in HW for
 	 * performance cluster cores. Enable it via SW to reduce power
 	 * impact.
 	 */
+
 	if (enable_dynamic_clock_gating) {
 
 		/* Skip enabling L1/L2 clock gating if perf l2 is not in low
 		 * power mode.
 		 */
-
 		status = (__raw_readl(l2_pwr_sts) & L2_HS_STS_SET)
 							== L2_HS_STS_SET;
 		if (status) {
@@ -102,21 +101,23 @@ static void process_lpm_workarounds(struct work_struct *w)
 			return;
 		}
 
-		l2_status = __raw_readl(l2_pwr_sts);
+		cpu = get_cpu();
 
+		l2_status = __raw_readl(l2_pwr_sts);
 		pr_err("Set L1_L2_GCC from cpu%d when perf L2 status=0x%x\n",
-			smp_processor_id(), l2_status);
+			cpu, l2_status);
 
 		if (is_l1_l2_gcc_secure) {
 			scm_io_write((u32)(l1_l2_gcc_res->start), 0x0);
-			if (scm_io_read((u32)(l1_l2_gcc_res->start)) !=0)
+			if (scm_io_read((u32)(l1_l2_gcc_res->start)) != 0)
 				pr_err("Failed to set L1_L2_GCC\n");
-		}
-		else {
+		} else {
 			__raw_writel(0x0, l1_l2_gcc);
 			if (__raw_readl(l1_l2_gcc) != 0x0)
 				pr_err("Failed to set L1_L2_GCC\n");
 		}
+
+		put_cpu();
 
 		HOTPLUG_NO_MITIGATION(&curr_req.offline_mask);
 		ret = devmgr_client_request_mitigation(
