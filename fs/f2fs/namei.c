9d5ef08069c642f6406765a7c1053b1dab4c816b
Jaegeuk Kim <jaegeuk@kernel.org>
2016-03-30 13:13:16 -0700

    f2fs crypto: fix corrupted symlink in encrypted case
    In the encrypted symlink case, we should check its corrupted symname after
    decrypting it.
    Otherwise, we can report -ENOENT incorrectly, if encrypted symname starts with
    '\0'.
    
    Cc: stable 4.5+ <stable@vger.kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/namei.c
+++ fs/f2fs/namei.c
@@ -764,14 +764,7 @@ static void *f2fs_encrypted_follow_link(struct dentry *dentry,
 		goto errout;
 	}
 
-	/* this is broken symlink case */
-	if (unlikely(cstr.name[0] == 0)) {
-		res = -ENOENT;
-		goto errout;
-	}
-
-	if ((cstr.len + sizeof(struct f2fs_encrypted_symlink_data) - 1) >
-								max_size) {
+	if ((cstr.len + sizeof(struct fscrypt_symlink_data) - 1) > max_size) {
 		/* Symlink data on the disk is corrupted */
 		res = -EIO;
 		goto errout;
@@ -783,6 +776,12 @@ static void *f2fs_encrypted_follow_link(struct dentry *dentry,
 	res = f2fs_fname_disk_to_usr(inode, NULL, &cstr, &pstr);
 	if (res < 0)
 		goto errout;
+
+	/* this is broken symlink case */
+	if (unlikely(pstr.name[0] == 0)) {
+		res = -ENOENT;
+		goto errout;
+	}
 
 	paddr = pstr.name;
 
