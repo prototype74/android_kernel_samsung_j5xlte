b5ca27c622b8595e785fc1f14d875d932edd5bd5
prototype74 <prototype74@outlook.de>
2023-12-23 20:15:42 +0100

    net: core: fib_rules: fix implicit declarations

--- net/core/fib_rules.c
+++ net/core/fib_rules.c
@@ -17,11 +17,6 @@
 #include <net/sock.h>
 #include <net/fib_rules.h>
 
-#define uid_valid(uid) ((uid) != -1)
-#define uid_lte(a, b) ((a) <= (b))
-#define uid_eq(a, b) ((a) == (b))
-#define uid_gte(a, b) ((a) >= (b))
-
 static const struct fib_kuid_range fib_kuid_range_unset = {
 	KUIDT_INIT(0),
 	KUIDT_INIT(~0),
@@ -656,12 +651,6 @@ static int fib_nl_fill_rule(struct sk_buff *skb, struct fib_rule *rule,
 	    (uid_range_set(&rule->uid_range) &&
 	     nla_put_uid_range(skb, &rule->uid_range)))
 		goto nla_put_failure;
-
-	if (uid_valid(rule->uid_start))
-	     nla_put_uid(skb, FRA_UID_START, rule->uid_start);
-
-	if (uid_valid(rule->uid_end))
-	     nla_put_uid(skb, FRA_UID_END, rule->uid_end);
 
 	if (ops->fill(rule, skb, frh) < 0)
 		goto nla_put_failure;
