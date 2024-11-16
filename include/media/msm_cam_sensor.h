5f092f8f1d65e225d5a49394f5cb7b7d3320f090
prototype74 <prototype74@outlook.de>
2024-11-16 18:13:17 +0100

    include: msm: camera: remove unnecessary arrays
    those arrays are unused and not supported by mm-camera, so remove them

--- include/media/msm_cam_sensor.h
+++ include/media/msm_cam_sensor.h
@@ -457,10 +457,8 @@ struct msm_sensor_power_setting {
 };
 
 struct msm_sensor_power_setting_array {
-	struct msm_sensor_power_setting  power_setting_a[MAX_POWER_CONFIG];
 	struct msm_sensor_power_setting *power_setting;
 	uint16_t size;
-	struct msm_sensor_power_setting  power_down_setting_a[MAX_POWER_CONFIG];
 	struct msm_sensor_power_setting *power_down_setting;
 	uint16_t size_down;
 };
@@ -563,7 +561,6 @@ struct msm_camera_csid_vc_cfg {
 
 struct msm_camera_csid_lut_params {
 	uint8_t num_cid;
-	struct msm_camera_csid_vc_cfg vc_cfg_a[MAX_CID];
 	struct msm_camera_csid_vc_cfg *vc_cfg[MAX_CID];
 };
 
@@ -756,11 +753,8 @@ struct msm_sensor_power_setting32 {
 };
 
 struct msm_sensor_power_setting_array32 {
-	struct msm_sensor_power_setting32 power_setting_a[MAX_POWER_CONFIG];
 	compat_uptr_t power_setting;
 	uint16_t size;
-	struct msm_sensor_power_setting32
-		power_down_setting_a[MAX_POWER_CONFIG];
 	compat_uptr_t power_down_setting;
 	uint16_t size_down;
 };
@@ -782,7 +776,6 @@ struct msm_camera_sensor_slave_info32 {
 
 struct msm_camera_csid_lut_params32 {
 	uint8_t num_cid;
-	struct msm_camera_csid_vc_cfg vc_cfg_a[MAX_CID];
 	compat_uptr_t vc_cfg[MAX_CID];
 };
 
