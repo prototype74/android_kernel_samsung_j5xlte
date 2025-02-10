4dd7672f100d89a7d4d500050b2ca74bcb5a8a17
prototype74 <prototype74@outlook.de>
2025-02-10 19:44:17 +0100

    mdss: S6E8AA5X01_AMS497HY01: add night_mode support
    This adds support for Samsung's Blue Light Filter to
    S6E8AA5X01_AMS497HY01 panels

--- drivers/video/msm/mdss/samsung/S6E8AA5X01_AMS497HY01_J5LTE/ss_dsi_panel_S6E8AA5X01_AMS497HY01.c
+++ drivers/video/msm/mdss/samsung/S6E8AA5X01_AMS497HY01_J5LTE/ss_dsi_panel_S6E8AA5X01_AMS497HY01.c
@@ -620,6 +620,8 @@ static void dsi_update_mdnie_data(void)
 	mdnie_data.DSI0_BYPASS_MDNIE = DSI0_BYPASS_MDNIE;
 	mdnie_data.DSI0_NEGATIVE_MDNIE = DSI0_NEGATIVE_MDNIE;
 	mdnie_data.DSI0_COLOR_BLIND_MDNIE = DSI0_COLOR_BLIND_MDNIE;
+	mdnie_data.DSI0_NIGHT_MODE_MDNIE = DSI0_NIGHT_MODE_MDNIE;
+	mdnie_data.DSI0_NIGHT_MODE_MDNIE_1 = DSI0_NIGHT_MODE_MDNIE_2;
 	mdnie_data.DSI0_HBM_CE_MDNIE = DSI0_HBM_CE_MDNIE;
 	mdnie_data.DSI0_HBM_CE_TEXT_MDNIE = DSI0_HBM_CE_TEXT_MDNIE;
 	mdnie_data.DSI0_RGB_SENSOR_MDNIE = DSI0_RGB_SENSOR_MDNIE;
@@ -688,6 +690,8 @@ static void dsi_update_mdnie_data(void)
 	mdnie_data.address_scr_white[ADDRESS_SCR_WHITE_BLUE_OFFSET] = ADDRESS_SCR_WHITE_BLUE;
 	mdnie_data.dsi0_rgb_sensor_mdnie_1_size = DSI0_RGB_SENSOR_MDNIE_1_SIZE;
 	mdnie_data.dsi0_rgb_sensor_mdnie_2_size = DSI0_RGB_SENSOR_MDNIE_2_SIZE;
+	mdnie_data.dsi0_night_mode_table = night_mode_data;
+	mdnie_data.dsi0_max_night_mode_index = 11;
 }
 
 static void mdss_panel_init(struct samsung_display_driver_data *vdd)
