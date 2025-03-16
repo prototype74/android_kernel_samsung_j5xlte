ef6d2abec6a7b801a42bae8ec8a7b4318b0bca63
prototype74 <prototype74@outlook.de>
2025-03-16 14:38:40 +0100

    nfc: pn547: support new power control ioctl
    New PN548AD implementation in userspace expects data_type long
    for PN547_SET_PWR. For backward compatibility keep old ioctl cmd
    as PN547_SET_PWR_OLD

--- include/linux/pn547.h
+++ include/linux/pn547.h
@@ -24,7 +24,8 @@
  * PN544_SET_PWR(1): power on
  * PN544_SET_PWR(>1): power on with firmware download enabled
  */
-#define PN547_SET_PWR	_IOW(PN547_MAGIC, 0x01, unsigned int)
+#define PN547_SET_PWR	_IOW(PN547_MAGIC, 0x01, long)
+#define PN547_SET_PWR_OLD	_IOW(PN547_MAGIC, 0x01, unsigned int)
 
 #ifdef CONFIG_NFC_PN547_ESE_SUPPORT
 /*
