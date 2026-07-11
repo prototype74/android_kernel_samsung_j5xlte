efb7e26e24b25a716cb97c92c61674ef9c18239b
prototype74 <prototype74@outlook.de>
2026-07-11 18:00:08 +0200

    msm: vb2: backport missing stream_rwlock protection
    Backport the stream_rwlock locking fixes from LA.BR.1.3.7.c26.
    
    These changes prevent lock imbalance and protect stream lookups against
    concurrent access. It appears that Samsung missed parts of this upstream
    fix.

--- drivers/media/platform/msm/camera_v2/msm_vb2/msm_vb2.c
+++ drivers/media/platform/msm/camera_v2/msm_vb2/msm_vb2.c
@@ -47,13 +47,11 @@ int msm_vb2_buf_init(struct vb2_buffer *vb)
 	struct msm_vb2_buffer *msm_vb2_buf;
 	unsigned long rl_flags;
 
-
 	session = msm_get_session_from_vb2q(vb->vb2_queue);
 	if (IS_ERR_OR_NULL(session))
 		return -EINVAL;
 
 	read_lock_irqsave(&session->stream_rwlock, rl_flags);
-
 
 	stream = msm_get_stream_from_vb2q(vb->vb2_queue);
 	if (!stream) {
@@ -114,7 +112,7 @@ static int msm_vb2_buf_finish(struct vb2_buffer *vb)
 		pr_err("%s:%d] vb2_buf NULL", __func__, __LINE__);
 		return -EINVAL;
 	}
-	
+
 	session = msm_get_session_from_vb2q(vb->vb2_queue);
 	if (IS_ERR_OR_NULL(session))
 		return -EINVAL;
@@ -145,7 +143,8 @@ static void msm_vb2_buf_cleanup(struct vb2_buffer *vb)
 {
 	struct msm_vb2_buffer *msm_vb2;
 	struct msm_stream *stream;
-	unsigned long flags;
+	struct msm_session *session;
+	unsigned long flags, rl_flags;
 
 	msm_vb2 = container_of(vb, struct msm_vb2_buffer, vb2_buf);
 
@@ -154,15 +153,23 @@ static void msm_vb2_buf_cleanup(struct vb2_buffer *vb)
 		return;
 	}
 
+	session = msm_get_session_from_vb2q(vb->vb2_queue);
+	if (IS_ERR_OR_NULL(session))
+		return;
+
+	read_lock_irqsave(&session->stream_rwlock, rl_flags);
+
 	stream = msm_get_stream_from_vb2q(vb->vb2_queue);
 	if (!stream) {
 		pr_err("%s:%d] NULL stream", __func__, __LINE__);
+		read_unlock_irqrestore(&session->stream_rwlock, rl_flags);
 		return;
 	}
 
 	spin_lock_irqsave(&stream->stream_lock, flags);
 	INIT_LIST_HEAD(&stream->queued_list);
 	spin_unlock_irqrestore(&stream->stream_lock, flags);
+	read_unlock_irqrestore(&session->stream_rwlock, rl_flags);
 }
 
 int msm_vb2_get_stream_state(struct msm_stream *stream)
@@ -180,7 +187,7 @@ int msm_vb2_get_stream_state(struct msm_stream *stream)
 	}
 	spin_unlock_irqrestore(&stream->stream_lock, flags);
 	return rc;
- }
+}
 
 static struct vb2_ops msm_vb2_get_q_op = {
 	.queue_setup	= msm_vb2_queue_setup,
@@ -200,6 +207,7 @@ static void *msm_vb2_dma_contig_get_userptr(void *alloc_ctx,
 	unsigned long vaddr, unsigned long size, int write)
 {
 	struct msm_vb2_private_data *priv;
+
 	priv = kzalloc(sizeof(*priv), GFP_KERNEL);
 	if (!priv)
 		return ERR_PTR(-ENOMEM);
@@ -234,8 +242,8 @@ static struct vb2_buffer *msm_vb2_get_buf(int session_id,
 	unsigned int stream_id)
 {
 	struct msm_stream *stream;
-	struct msm_session *session;
 	struct vb2_buffer *vb2_buf = NULL;
+	struct msm_session *session;
 	struct msm_vb2_buffer *msm_vb2 = NULL;
 	unsigned long flags, rl_flags;
 
@@ -310,6 +318,7 @@ static int msm_vb2_put_buf(struct vb2_buffer *vb, int session_id,
 			pr_err("VB buffer is INVALID vb=%pK, ses_id=%d, str_id=%d\n",
 					vb, session_id, stream_id);
 			spin_unlock_irqrestore(&stream->stream_lock, flags);
+			read_unlock_irqrestore(&session->stream_rwlock, rl_flags);
 			return -EINVAL;
 		}
 		msm_vb2 =
@@ -335,8 +344,8 @@ static int msm_vb2_buf_done(struct vb2_buffer *vb, int session_id,
 	unsigned long flags, rl_flags;
 	struct msm_vb2_buffer *msm_vb2;
 	struct msm_stream *stream;
-	struct msm_session *session;
 	struct vb2_buffer *vb2_buf = NULL;
+	struct msm_session *session;
 	int rc = 0;
 
 	session = msm_get_session(session_id);
@@ -362,6 +371,7 @@ static int msm_vb2_buf_done(struct vb2_buffer *vb, int session_id,
 			pr_err("VB buffer is INVALID ses_id=%d, str_id=%d, vb=%pK\n",
 				    session_id, stream_id, vb);
 			spin_unlock_irqrestore(&stream->stream_lock, flags);
+			read_unlock_irqrestore(&session->stream_rwlock, rl_flags);
 			return -EINVAL;
 		}
 		msm_vb2 =
